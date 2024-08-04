950bc91c9c6b99f70b955953060af461db5917df
Akaikenlol <keithken972@gmail.com>
2024-08-04 20:43:13 +0630

    feat: Implementing Live_blocks chore: npm i live blocks

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,25 +1,7 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
-	return (
-		<div>
-			<Header>
-				<div className="flex w-fit items-center justify-center gap-2">
-					<p className="document-title">Share</p>
-				</div>
-				<SignedOut>
-					<SignInButton />
-				</SignedOut>
-				<SignedIn>
-					<UserButton />
-				</SignedIn>
-			</Header>
-			<Editor />
-		</div>
-	);
+	return <div></div>;
 };
 
 export default Document;
